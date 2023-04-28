6902cfe58fe9bb6adf90f01bebf77586449dc38f
NullVoidNan <128657358+QuixoticLeprechauns@users.noreply.github.com>
2023-04-28 13:16:30 -0700

    stuff is happening

--- index.js
+++ index.js
@@ -34,9 +34,6 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
-client.login(token);
-
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
@@ -58,3 +55,6 @@ client.on(Events.InteractionCreate, async interaction => {
 		}
 	}
 });
+
+// Log in to Discord with your client's token
+client.login(token);
